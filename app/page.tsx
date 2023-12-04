f90080a724f32f6647d10051e83f3f0bb2ca6894
elvissamuel <77748602+elvissamuel@users.noreply.github.com>
2023-12-04 09:55:54 +0100

    Create NavBar

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="">
+    <main className=''>
       <div className='text-black'>
-        Hello world
+        Hello
       </div>
     </main>
   )
